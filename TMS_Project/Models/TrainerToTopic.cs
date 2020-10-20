0c8e268061b3fb48848100861e11c9c1af5c67a6
Long Do <dosylong3011@gmail.com>
2020-10-21 02:32:00 +0700

    Update

---
+++ TMS_Project/Models/TrainerToTopic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TMS_Project.Models
+{
+	public class TrainerToTopic
+	{
+		public int Id { get; set; }
+		public string TrainerId { get; set; }
+		public ApplicationUser Trainer { get; set; }
+		public int TopicId { get; set; }
+		public Topic Topic { get; set; }
+	}
+}
